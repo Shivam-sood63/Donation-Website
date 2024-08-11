93fdcb7d794a547bb8ddce2e24e1cbca05733a20
Shivam sood <155950691+Shivam-sood63@users.noreply.github.com>
2024-08-11 23:56:05 +0530

    Donation website project

---
+++ app.js
@@ -0,0 +1,384 @@
+gsap.from(".give-h1", {
+  y: 30,
+  duration: 1,
+  opacity: 0,
+});
+gsap.from(".give-para", {
+  y: 30,
+  duration: 1,
+  opacity: 0,
+});  
+gsap.from(".give-btn", {
+  y: 12,
+  duration: 1,
+  delay: 1,
+});
+
+gsap.from(".item2 p", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".item2 p",
+  },
+})
+gsap.from(".item2 h1", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".item2 h1",
+    scrub: 1,
+  },
+});
+gsap.from(".item2 h4", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".item2 h4",
+    scrub: 1,
+  },
+});
+gsap.to(".btn1", {
+  y: 10,
+  duration: 1,
+  delay: 0.1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".btn1",
+    scrub: 1,
+  },
+});
+gsap.to(".btn2", {
+  y: 10,
+  duration: 1,
+  delay: 0.1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".btn2",
+    scrub: 1,
+  },
+});
+gsap.from(".about1 p", {
+  y: 30,
+  duration: 1,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".about1 p",
+    scrub: 1,
+  },
+});
+gsap.from(".about1 h1", {
+  y: 40,
+  duration: 1,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".about1 h1",
+    scrub: 1,
+  },
+});
+gsap.from(".part1", {
+  y: 40,
+  duration: 1,
+  delay: 0.1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".part1",
+    scrub: 1,
+  },
+});
+gsap.from(".what1", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".what1",
+  },
+});
+gsap.from(".what1 p", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".what1 p",
+    scrub: 1,
+  },
+});
+gsap.from(".what1 h1", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: ".what1",
+  scrub: true,
+});
+gsap.from(".orange", {
+  y: 40,
+  duration: 2,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".orange",
+  },
+});
+gsap.from(".orange h1", {
+  y: 40,
+  duration: 1,
+  opacity: 0.2,
+  scrollTrigger: {
+    trigger: ".orange h1",
+    scrub: 1,
+  },
+});
+gsap.from(".orange p", {
+  y: 40,
+  duration: 1,
+  opacity: 0.2,
+  scrollTrigger: {
+    trigger: ".orange p",
+    scrub: 1,
+  },
+});
+gsap.from(".orange h3", {
+  y: 40,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".orange h3",
+    scrub: 1,
+  },
+});
+gsap.from(".pure1", {
+  y: 40,
+  duration: 1,
+  opacity: 0.2,
+  scrollTrigger: {
+    trigger: ".pure1",
+    scrub: 1,
+  },
+});
+gsap.from(".map", {
+  y: 10,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".map",
+  },
+});
+// ----------------------WHO WE ARE----------------SECOND PAGE START----------------------
+gsap.from(".page2 h1", {
+  y: 40,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".page2 h1",
+    scrub: 1,
+  },
+});
+gsap.from(".page2 p", {
+  y: 40,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".page2 p",
+    scrub: 1,
+  },
+});
+gsap.from(".we1", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0.6,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".we1",
+    scrub: 1,
+  },
+});
+gsap.from(".we2", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  scrollTrigger: {
+    trigger: ".we2",
+    scrub: 1,
+  },
+});
+gsap.from(".we2 h3", {
+  y: 10,
+  duration: 2,
+  delay: 0.1,
+  scrollTrigger: {
+    trigger: ".we2 h3",
+    scrub: 1,
+  },
+});
+gsap.from(".we2 p", {
+  y: 10,
+  duration: 2,
+  delay: 0.1,
+  scrollTrigger: {
+    trigger: ".we2 p",
+    scrub: 1,
+  },
+});
+
+// -----------------------PROGRAM PORTION START------------------THIRD PAGE  START---------------
+gsap.from(".about h1", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".about h1",
+    scrub: 1,
+  },
+});
+gsap.from(".about p", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".about p",
+    scrub: 1,
+  },
+});
+gsap.from(".about_new", {
+  y: 40,
+  duration: 2,
+  delay: 0.1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".about_new",
+    scrub: 1,
+  },
+});
+
+// -----------------ABOUT US ---------------FOURTH PAGE START-----------------
+
+gsap.from(".about-part h1", {
+  y: 20,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".about-part h1",
+    scrub: 1,
+  },
+});
+gsap.from(".abot h2", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".abot h2",
+    scrub: 1,
+  },
+});
+gsap.from(".abot p", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".abot p",
+    scrub: 1,
+  },
+});
+gsap.from(".abotchild h1", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".abotchild h1",
+    scrub: 1,
+  },
+});
+gsap.from(".abotchild h3", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".abotchild h3",
+    scrub: 1,
+  },
+});
+gsap.from(".abotchild p", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".abotchild p",
+    scrub: 1,
+  },
+});
+
+// -------------------------------BLOG PORTION ------------------FIFTH PAGE START----------
+gsap.from(".th h1", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".th h1",
+    scrub: 1,
+  },
+});
+gsap.from(".three1", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".three1",
+    scrub: 1,
+  },
+});
+gsap.from(".three2", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".three2",
+    scrub: 1,
+  },
+});
+
+// ------------------LAST PAGE OF THE WEBPAGE --------------------------------
+gsap.from(".touch h1", {
+  y: 40,
+  duration: 3,
+});
+gsap.from(".uss button", {
+  y: 40,
+  duration: 3,
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
